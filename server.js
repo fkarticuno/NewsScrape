c1b211b7a9b4d37f3caff54eb8633fcc29d0cab0
Frederick Articuno <mckiyoshi@gmail.com>
2019-12-17 21:29:19 -0500

    fixed the port?

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get("/scrape", function(req, res) {
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
+  db.Article.findOne({})
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
